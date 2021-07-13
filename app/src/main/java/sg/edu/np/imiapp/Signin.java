079c99be4f1174f3dab493de9c89e329639695c7
LuoYuXuan <reathan@126.com>
2021-07-14 00:25:38 +0800

    Update Login interface

--- app/src/main/java/sg/edu/np/imiapp/Signin.java
+++ app/src/main/java/sg/edu/np/imiapp/Signin.java
@@ -8,20 +8,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Signin extends AppCompatActivity {
-    public EditText userEmail;
-    public EditText userPassword;
+    public TextInputEditText userEmail;
+    public TextInputEditText userPassword;
     public ProgressBar loadingBar;
     private FirebaseAuth mAuth;
     @Override
@@ -32,13 +31,13 @@ public class Signin extends AppCompatActivity {
         //find email and password editText in activity_signin.xml
         this.userEmail = findViewById(R.id.userEmailSignIn);
         this.userPassword = findViewById(R.id.userPasswordSignIn);
-        loadingBar = findViewById(R.id.loadingBar);
+        loadingBar = findViewById(R.id.loadingBar2);
         loadingBar.setVisibility(View.GONE);
 
         TextView signUp = findViewById((R.id.signUpNow));
 
         //find createAccount button in activity_signin.xml
-        Button signIn =(Button) findViewById(R.id.userLogin);
+        TextView signIn = findViewById(R.id.userLogin);
 
         //initialise Firebase auth
         mAuth = FirebaseAuth.getInstance();
