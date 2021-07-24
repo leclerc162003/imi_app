7f64a0e0fa631d11dcc66a6fc838c94cafa214ec
leclerc162003 <S10203330@connect.np.edu.sg>
2021-07-24 22:51:38 +0800

    implemented change password

--- app/src/main/java/sg/edu/np/imiapp/Settings.java
+++ app/src/main/java/sg/edu/np/imiapp/Settings.java
@@ -2,7 +2,11 @@ package sg.edu.np.imiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        ImageView changePass = findViewById(R.id.changePassImage);
+        changePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Settings.this, changePassword.class);
+                Settings.this.startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/sg/edu/np/imiapp/changePassword.java
+++ app/src/main/java/sg/edu/np/imiapp/changePassword.java
@@ -2,17 +2,46 @@ package sg.edu.np.imiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class changePassword extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password2);
-        //mAuth.getCurrentUser().updatePassword();
+        EditText newPass = findViewById(R.id.updatePass);
+        EditText reEnteredPass = findViewById(R.id.reUpdatePass);
+        ImageView update = findViewById(R.id.updateButton);
+        mAuth = FirebaseAuth.getInstance();
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!reEnteredPass.getText().toString().contentEquals(newPass.getText().toString())){
+                    reEnteredPass.setError("Password does not match");
+                }
+                else{
+                    mAuth.getCurrentUser().updatePassword(reEnteredPass.getText().toString());
+                    Toast.makeText(changePassword.this, "You have successfully changed password.", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(changePassword.this, Signin.class);
+                    changePassword.this.startActivity(i);
+                    mAuth.signOut();
+                }
+            }
+        });
+
+
 
     }
 }
